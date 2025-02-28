0e14bf8752e7222746303b7303fca8e288713b6f
3zDax42 <ezri@thebandit.org>
2025-02-28 15:37:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,48 @@ pygame.init()
 pygame.display.set_caption("Space_Invaders!")
 Game_Screen = pygame.display.set_mode((800,800))
 Tick_Speed = pygame.time.Clock()
+Time = 0
 Playing_Game = True
+enemy1 = pygame.image.load('Pipe.png').convert_alpha()
 #------------------------------------------------------------------### Classes ###
+class Alien:
+    def __init__(self, X_Pos, Y_Pos):
+        self.X_Pos = X_Pos
+        self.Y_Pos = Y_Pos
+        self.Living = True
+        self.Direction = 1
+        self.Width
+        self.Height
+    def Move(self, Time):
+        if Time % 800 == 0:
+            
+            self.Direction *= -1
+        if Time % 100==0:
+            self.X_Pos += 50
+    def Draw(self):
+        pass
+
+# Armada = []
+# for i in range (2):
+#     for j in range(4):
+#         Armada.append(Alien(j*60+50, i*50+50))
+class Bullet:
+    def __init__(self, X_Pos, Y_Pos):
+        self.X_Pos = X_Pos+28
+        self.Y_Pos = Y_Pos-15
+        self.Living = False
+    def Move(self, X_Pos, Y_Pos):
+        if self.Living == True:
+            self.Y_Pos -= 5
+            #print("Bullet True")
+        if self.Y_Pos < 0:
+            self.Living = False
+        if self.Living == False:
+            self.X_Pos = X_Pos+28
+            self.Y_Pos = Y_Pos-15
+    def Draw(self):
+        pygame.draw.rect(Game_Screen, (250,250,250), (self.X_Pos, self.Y_Pos, 3, 10))
+
 class Player:
     def __init__(self):
         self.X_Pos = 400
@@ -12,17 +52,22 @@ class Player:
         self.X_Vol = 0
         self.MoveLeft = False
         self.MoveRight = False
-    def Input(self):
+        self.Shoot = False
+    def Input(self, Key1, Key2, Key3):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+            if event.key == Key1:
                 self.MoveLeft = True
-            elif event.key == pygame.K_RIGHT:
+            elif event.key == Key2:
                 self.MoveRight = True
+            elif event.key == Key3:
+                self.Shoot = True
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
+            if event.key == Key1:
                 self.MoveLeft = False
-            elif event.key == pygame.K_RIGHT:
+            elif event.key == Key2:
                 self.MoveRight = False
+            elif event.key == Key3:
+                self.Shoot = False
     def Physics(self):
         if self.MoveLeft:
             self.X_Vol = -3
@@ -38,16 +83,23 @@ class Player:
         pygame.draw.rect(Game_Screen, (200, 200, 100), (self.X_Pos+27, 735, 6, 35))
 #--------------------------------------------------------------------#
 player = Player()
-#--------------------------------------------------------------------#
+bullet = Bullet(player.X_Pos, player.Y_Pos)
+#--------------------------------------------------------------------################-------------------------------------------------------#
 while Playing_Game:
     Tick_Speed.tick(60)
+    Time += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Playing_Game = False
-        player.Input()
+        player.Input(pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP)
     player.Physics()
+    if player.Shoot == True:
+        bullet.Living = True
+    bullet.Move(player.X_Pos, player.Y_Pos)
 #--------------------------------------------------------------------### Render Section ###
     Game_Screen.fill((0, 0, 0))
+    if bullet.Living == True:
+        bullet.Draw()
     player.Draw()
     pygame.display.flip()
 
